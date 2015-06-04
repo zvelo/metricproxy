0ef94631ee685322e5a259dad39443609d8336bb
Joshua Rubin <jrubin@zvelo.com>
2015-06-04 10:07:39 -0600

    disable metricproxy stats
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- etcd.sh
+++ etcd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-etcdctl set /config/service/metricproxy/stats-delay '"1s"' # or null if dne
+etcdctl set /config/service/metricproxy/stats-delay null # or duration, with quotes, like '"1s"'
 etcdctl set /config/service/metricproxy/listen/timeout 30s
 #etcdctl set /config/service/metricproxy/forward/signalfx/token
 etcdctl set /config/service/metricproxy/forward/signalfx/url https://ingest.signalfx.com/v2/datapoint
